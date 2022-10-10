ca5ebf62282507f3a66942012ee3e6c76029c5c5
German Dollinger <german.dollinger@gmail.com>
2022-10-10 16:35:35 -0300

    burguer java

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,38 @@
-/*productos*/
-let nombreProductoA = 'Carne'
-let nombreProductoB = 'Pollo'
-let nombrePrductoC = 'Vegetariana'
+//productos básicos ordenados en objetos
 
-let precioProducto = 600
+let productoA = { nombre: "Carne", stock: 1000, precio: 1000 }
+let productoB = { nombre: "Pollo", stock: 2000, precio: 900 }
+let productoC = { nombre: "Vegetariana", stock: 500, precio: 800 }
 
-let complementoProductoA = 'lechuga'
-let complementoProductoB = 'tomate'
-let complementoProductoC = 'pepino'
-let complementoProductoD = 'cebolla'
-let complementoProductoE = 'morron'
-let complementoProductoF = 'chedar'
-let complementoProductoG = 'huevo frito'
-let complementoProductoH = 'jamon'
-let complementoProductoI = 'panceta'
+//productos para complementar la hamburguesa ordenados en objetos
 
+let complementoProductoA = { nombre: "lechuga", stock: 500, precio: 50 }
+let complementoProductoB = { nombre: "tomate", stock: 600, precio: 100 }
+let complementoProductoc = { nombre: "pepino", stock: 1000, precio: 50 }
+let complementoProductoD = { nombre: "cebolla", stock: 500, precio: 50 }
+let complementoProductoE = { nombre: "morron", stock: 700, precio: 100 }
+let complementoProductoF = { nombre: "chedar", stock: 800, precio: 100 }
+let complementoProductoG = { nombre: "huevo frito", stock: 800, precio: 200 }
+let complementoProductoH = { nombre: "jamon", stock: 500, precio: 200 }
+let complementoProductoI = { nombre: "panceta", stock: 600, precio: 200 }
+
+let precioTotal = 0
+
+//listas con arrays
+
+let listaProductos = [productoA, productoB, productoC] 
+let listaComplementosProducto = [complementoProductoA, complementoProductoB, complementoProductoc,          complementoProductoD, complementoProductoE, complementoProductoF, complementoProductoG, complementoProductoH, complementoProductoI ]
+
+//filtro de nombres
+
+let nombresListaProductos = listaProductos.map ((el) => el.nombre)
+let nombresListaComplementosProductos = listaComplementosProducto.map ((el) => el.nombre)
+
+//funciones
 
 function complemento (){
-let complementoElegido = prompt ("Cómo desea complementar su hamburguesa de " + entrada + "\n - lechuga\n - tomate\n - pepino\n - cebolla\n - morron\n - chedar\n - huevo frito\n - jamon\n - panceta\n - ESC-Salir" ) ;  
+
+let complementoElegido = prompt ("Cómo desea complementar su hamburguesa de \n - " + nombresListaComplementosProductos.join (" \n - ") + "\n - ESC-Salir" ) ;  
         if (complementoElegido.toUpperCase() != 'ESC'){
             switch (complementoElegido) {
                 case "lechuga" :
@@ -52,24 +67,51 @@ let complementoElegido = prompt ("Cómo desea complementar su hamburguesa de " +
                     break;
         }  }
         else { 
+            alert ("Gracias lo esperamos");}}
 
-            alert ("Gracias lo esperamos");
+function calculoPrecio (cantidad, precio) {
+    precioTotal += (cantidad * precio)
+}
+
+function calculoStock (cantidad, stock, precio){
+    if(cantidad <= stock){
+        complemento () ;       
+        calculoPrecio (cantidad, precio);
+            alert ("el total es: " + precioTotal)
+            alert ("que Desea seguir eligiendo")
         }
-    
-    }
+        else{
+            alert("Actualmente tenemos " + stock + " unidades de este producto")
+        }}
+
+//ciclo de compra
 
 
-
-let entrada = prompt ("Cómo desea armar su Hamburguesa\n - Carne\n - Lenteja\n - Pollo\n - ESC-Salir") ;
+let entrada = prompt ("Cómo desea armar su Hamburguesa: \n - " + nombresListaProductos.join(" \n - ") + " \n - ESC-Salir") ;
 while (entrada != "ESC") {
     if(entrada.toUpperCase() == 'CARNE'){
-        complemento () ;       
-                alert ("que Desea seguir eligiendo")
-            }
+        let cantidadProductoCarne = parseInt(prompt("ingrese que cantidad de hamburguesas de " + entrada + " desea comprar:"))
+        calculoStock (cantidadProductoCarne, productoA.stock, productoA.precio )
+        productoA.stock -= cantidadProductoCarne
+        }
     else if (entrada.toUpperCase() == 'POLLO' ) {
-        complemento () ;       
-        
+        let cantidadProductoPollo = parseInt(prompt("ingrese que cantidad de hamburguesas de " + entrada + " desea comprar:"))
+        calculoStock (cantidadProductoPollo, productoB.stock, productoB.precio )             
             }
     else if (entrada.toUpperCase() == 'LENTEJA' ) {
-        complemento () ;       
-            } }
+        let cantidadProductoLenteja = parseInt(prompt("ingrese que cantidad de hamburguesas de " + entrada + " desea comprar:"))
+        calculoStock (cantidadProductoLenteja, productoC.stock, productoC.precio )       
+            } 
+    else {
+            alert("Por favor elija el producto correspondiente");
+        }}
+
+
+
+
+if(precioTotal != 0){         
+alert("El precio total es: " + precioTotal)
+}
+else{
+alert("Gracias por su visita!")
+}
